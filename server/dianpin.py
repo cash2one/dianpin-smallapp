d55b6b3ef24e0b34f48776f73438373c102063c2
yeah529 <samwong90529@gmail.com>
2017-10-18 17:24:48 +0800

    edited

--- server/dianpin.py
+++ server/dianpin.py
@@ -6,7 +6,7 @@
 import numpy as np
 import re
 import math
-
+import io
 
 from keras.models import Sequential
 from keras.layers import Dense, Activation, LSTM,Embedding,Dropout
@@ -59,77 +59,6 @@ def encode_io_pairs(text,window_size,step_size,chars_to_indices):
 
 # In[3]:
 
-import io
-with io.open("/resources/dict.txt", "r", encoding="utf-8") as my_file:
-     text = my_file.read() 
-my_file.close()
-
-
-# In[4]:
-
-print('our original text has ' + str(len(text)) + ' characters')
-
-
-# In[5]:
-
-text = text.replace(u'\xa0','')
-text = text.replace(u'    ','')
-text = text.replace(u'\n','')
-
-
-# In[6]:
-
-#去除emoj表情和一些很奇怪的隐藏符号
-text = "".join(re.findall(u'[a-z0-9\w\u2E80-\u9FFF，。！？\-\\.\!\?\;\:\'\#]*',text))
-
-
-# In[7]:
-
-len(list(set(text)))
-
-
-# In[8]:
-
-text = text[int(math.ceil(len(text)*0.75)):]
-print(len(text))
-
-
-# In[9]:
-
-chars = sorted(list(set(text)))
-print ("this corpus has " +  str(len(chars)) + " unique characters")
-# this dictionary is a function mapping each unique character to a unique integer
-chars_to_indices = dict((c, i) for i, c in enumerate(chars))  # map each unique character to unique integer
-
-# this dictionary is a function mapping each unique integer back to a unique character
-indices_to_chars = dict((i, c) for i, c in enumerate(chars))  # map each unique integer back to unique character
-
-
-# In[10]:
-
-window_size = 7
-step_size = 2
-X,y = encode_io_pairs(text,window_size,step_size,chars_to_indices)
-
-
-# ## 模型建立
-
-# In[11]:
-
-model = Sequential()
-model.add(LSTM(128, input_shape=(window_size,len(chars))))
-model.add(Dense(len(chars),activation='softmax'))
-
-optimizer = keras.optimizers.RMSprop(lr=0.001, rho=0.9, epsilon=1e-08, decay=1e-06)
-model.compile(loss='categorical_crossentropy', optimizer=optimizer)
-
-
-# ## Load weights and Retrain the model
-
-# In[12]:
-
-# load in weights
-model.load_weights('resources/best_RNN_textdata_weights.hdf5')
 
 
 # In[28]:
@@ -221,53 +150,123 @@ def predict_next_chars(model,num_to_predict,window_size,chars_to_indices,indices
 
 # In[47]:
 
-print(predict_next_chars(model,80,window_size,chars_to_indices,indices_to_chars,chars))
+
 
 
 # # 第二个model只针对三个词
 
 # In[13]:
 
-with io.open("./data/dict.txt", "r", encoding="utf-8") as my_file:
-     text2 = my_file.read() 
-my_file.close()
-
-text2 = text2.replace(u'\xa0','')
-text2 = text2.replace(u'    ','')
-text2 = text2.replace(u'\n','')
-#去除emoj表情和一些很奇怪的隐藏符号
-text2 = "".join(re.findall(u'[a-z0-9\w\u2E80-\u9FFF，。！？\-\\.\!\?\;\:\'\#]*',text2))
-text2 = text2[:int(math.ceil(len(text2)*0.15))]
-print(len(text2))
-
-chars2 = sorted(list(set(text2)))
-print ("this corpus has " +  str(len(chars2)) + " unique characters")
-# this dictionary is a function mapping each unique character to a unique integer
-chars_to_indices2 = dict((c, i) for i, c in enumerate(chars2))  # map each unique character to unique integer
-
-# this dictionary is a function mapping each unique integer back to a unique character
-indices_to_chars2 = dict((i, c) for i, c in enumerate(chars2))  # map each unique integer back to unique character
-
-
-# In[14]:
-
-window_size2 = 3
-step_size2 = 1
-X2,y2 = encode_io_pairs(text2,window_size2,step_size2,chars_to_indices2)
-
-
-# In[15]:
-
-model2 = Sequential()
-model2.add(LSTM(128, input_shape=(window_size2,len(chars2))))
-#model.add(Dropout(0,2))
-model2.add(Dense(len(chars2),activation='softmax'))
-
-optimizer2 = keras.optimizers.RMSprop(lr=0.001, rho=0.9, epsilon=1e-08, decay=1e-06)
-model2.compile(loss='categorical_crossentropy', optimizer=optimizer2)
 
 
 # In[16]:
 
-model2.load_weights('resources/best_RNN2_textdata_weights.hdf5')
 
+
+
+if __name__ == '__main__':
+	
+	
+	
+	with io.open("./resources/dict.txt", "r", encoding="utf-8") as my_file:
+		 text = my_file.read() 
+	my_file.close()
+
+
+	# In[4]:
+
+	print('our original text has ' + str(len(text)) + ' characters')
+
+
+	# In[5]:
+
+	text = text.replace(u'\xa0','')
+	text = text.replace(u'    ','')
+	text = text.replace(u'\n','')
+
+
+	# In[6]:
+
+	#去除emoj表情和一些很奇怪的隐藏符号
+	text = "".join(re.findall(u'[a-z0-9\w\u2E80-\u9FFF，。！？\-\\.\!\?\;\:\'\#]*',text))
+
+
+
+
+	# In[8]:
+
+	text = text[int(math.ceil(len(text)*0.75)):]
+	print(len(text))
+
+
+	# In[9]:
+
+	chars = sorted(list(set(text)))
+	print ("this corpus has " +  str(len(chars)) + " unique characters")
+	# this dictionary is a function mapping each unique character to a unique integer
+	chars_to_indices = dict((c, i) for i, c in enumerate(chars))  # map each unique character to unique integer
+
+	# this dictionary is a function mapping each unique integer back to a unique character
+	indices_to_chars = dict((i, c) for i, c in enumerate(chars))  # map each unique integer back to unique character
+
+
+
+	window_size = 7
+	step_size = 2
+	X,y = encode_io_pairs(text,window_size,step_size,chars_to_indices)
+
+
+
+	model = Sequential()
+	model.add(LSTM(128, input_shape=(window_size,len(chars))))
+	model.add(Dense(len(chars),activation='softmax'))
+
+	optimizer = keras.optimizers.RMSprop(lr=0.001, rho=0.9, epsilon=1e-08, decay=1e-06)
+	model.compile(loss='categorical_crossentropy', optimizer=optimizer)
+	
+
+	
+	with io.open("./resources/dict.txt", "r", encoding="utf-8") as my_file:
+		 text2 = my_file.read() 
+	my_file.close()
+
+	text2 = text2.replace(u'\xa0','')
+	text2 = text2.replace(u'    ','')
+	text2 = text2.replace(u'\n','')
+	#去除emoj表情和一些很奇怪的隐藏符号
+	text2 = "".join(re.findall(u'[a-z0-9\w\u2E80-\u9FFF，。！？\-\\.\!\?\;\:\'\#]*',text2))
+	text2 = text2[:int(math.ceil(len(text2)*0.15))]
+	print(len(text2))
+
+	chars2 = sorted(list(set(text2)))
+	print ("this corpus has " +  str(len(chars2)) + " unique characters")
+	# this dictionary is a function mapping each unique character to a unique integer
+	chars_to_indices2 = dict((c, i) for i, c in enumerate(chars2))  # map each unique character to unique integer
+
+	# this dictionary is a function mapping each unique integer back to a unique character
+	indices_to_chars2 = dict((i, c) for i, c in enumerate(chars2))  # map each unique integer back to unique character
+
+
+	# In[14]:
+
+	window_size2 = 3
+	step_size2 = 1
+	X2,y2 = encode_io_pairs(text2,window_size2,step_size2,chars_to_indices2)
+
+
+	# In[15]:
+
+	model2 = Sequential()
+	model2.add(LSTM(128, input_shape=(window_size2,len(chars2))))
+	#model.add(Dropout(0,2))
+	model2.add(Dense(len(chars2),activation='softmax'))
+
+	optimizer2 = keras.optimizers.RMSprop(lr=0.001, rho=0.9, epsilon=1e-08, decay=1e-06)
+	model2.compile(loss='categorical_crossentropy', optimizer=optimizer2)
+
+
+	model.load_weights('./resources/best_RNN_textdata_weights.hdf5')
+	model2.load_weights('./resources/best_RNN2_textdata_weights.hdf5')
+
+	#final Output
+	print(predict_next_chars(model,80,window_size,chars_to_indices,indices_to_chars,chars))

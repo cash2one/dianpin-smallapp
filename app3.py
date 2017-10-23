3b16d78dbdfbb9428372742bbf31063f913138c9
root <samwong90529@gmail.com>
2017-10-23 10:05:31 +0800

    edit

---
+++ app3.py
@@ -0,0 +1,24 @@
+from flask import Flask
+from server import dianpin
+
+app = Flask(__name__)
+
+model = None
+@app.route('/dianpin')
+def create_model():
+	global model
+	model = dianpin.Dianpin()
+	model.model_built()
+	return "Model Loaded"
+	
+@app.route('/predict')
+def predict():
+	global model
+	return model.final_predict()
+
+@app.route('/')
+def hello_world():
+	return 'Hello, World!'
+
+if __name__ == '__main__':
+	app.run('0.0.0.0',part=80)

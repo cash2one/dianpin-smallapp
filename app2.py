c6e7cc998a10a8f6c00db2e9758d2fb7c560d370
yeah529 <samwong90529@gmail.com>
2017-10-26 11:24:50 +0800

    edited

--- app2.py
+++ app2.py
@@ -4,7 +4,7 @@ with tf.device('/gpu:0'):
 	a = tf.constant([1.0, 2.0, 3.0, 4.0, 5.0, 6.0], shape=[2, 3], name='a')
 	b = tf.constant([1.0, 2.0, 3.0, 4.0, 5.0, 6.0], shape=[3, 2], name='b')
 	c = tf.matmul(a, b)
-	# 新建session with log_device_placement并设置为True.
+
 	sess = tf.Session(config=tf.ConfigProto(log_device_placement=True))
-	# 运行这个 op.
+
 	print sess.run(c)

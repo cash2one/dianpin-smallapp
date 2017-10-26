cde51e056dfad763e3afa07902da83c9bcd47a8c
yeah529 <samwong90529@gmail.com>
2017-10-26 12:58:54 +0800

    lr added

--- server/char_rnn/model.py
+++ server/char_rnn/model.py
@@ -100,10 +100,10 @@ class CharRNN:
         
         global_step = tf.Variable(0, trainable=False)
         
-        learning_rate = tf.train.exponential_decay(self.learning_rate, global_step,
+        lr = tf.train.exponential_decay(self.learning_rate, global_step,
  		5000, 0.96, staircase=True)
         
-        train_op = tf.train.AdamOptimizer(self.learning_rate)
+        train_op = tf.train.AdamOptimizer(lr)
         self.optimizer = train_op.apply_gradients(zip(grads, tvars),global_step=global_step)
 
     def train(self, batch_generator, max_steps, save_path, save_every_n, log_every_n):

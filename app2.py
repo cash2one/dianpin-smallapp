67776db00bdebc2c9bdcee89fb2e6945dc3318ca
yeah529 <samwong90529@gmail.com>
2017-10-26 11:19:36 +0800

    try gpu

--- app2.py
+++ app2.py
@@ -1,21 +1,9 @@
-from flask import Flask
-from server import dianpin
+import tensorflow as tf
 
-app = Flask(__name__)
-
-model = None
-@app.route('/dianpin')
-def create_model():
-	global model
-	model = dianpin.Dianpin()
-	model.model_built()
-	return "Model Loaded"
-	
-@app.route('/predict')
-def predict():
-	global model
-	return model.final_predict()
-
-@app.route('/')
-def hello_world():
-	return 'Hello, World!'
+a = tf.constant([1.0, 2.0, 3.0, 4.0, 5.0, 6.0], shape=[2, 3], name='a')
+b = tf.constant([1.0, 2.0, 3.0, 4.0, 5.0, 6.0], shape=[3, 2], name='b')
+c = tf.matmul(a, b)
+# 新建session with log_device_placement并设置为True.
+sess = tf.Session(config=tf.ConfigProto(log_device_placement=True))
+# 运行这个 op.
+print sess.run(c)
